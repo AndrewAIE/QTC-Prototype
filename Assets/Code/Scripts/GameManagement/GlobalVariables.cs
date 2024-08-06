d276f937102e5eb57551d2decf0b391d140f04fb
Oskar <s200080@students.aie.edu.au>
2024-08-06 15:59:43 +0930

    qte works with scriptable object

---
+++ Assets/Code/Scripts/GameManagement/GlobalVariables.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace QTCGlobals
+{
+    public enum QTEState
+    {
+        Waiting,
+        Running,
+        Complete,
+        Failed
+    }
+    /// <summary>
+    /// Holds the Input and how many times it needs to be pressed
+    /// </summary>
+    [System.Serializable]
+    public struct QTEINPUTS
+    {
+        /// <summary>
+        /// inputs needed, inputs as North, East, South, West
+        /// </summary>
+        public bool[] inputs;
+        /// <summary>
+        /// how many times the button needs to be pressed
+        /// </summary>
+        public float mashAmount;
+        /// <summary>
+        /// seconds to complete
+        /// </summary>
+        public float TimeToComplete;
+    }
+    /// <summary>
+    /// holds all QTEINPUTS and the time to complete the QTE
+    /// </summary>
+    [System.Serializable]
+    public struct QTESTREAM
+    {
+        /// <summary>
+        /// all required input combos
+        /// </summary>
+        public QTEINPUTS[] qteInputs;
+        
+    }
+}
